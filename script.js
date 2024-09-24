6e7b24a9cb095546ca38492b5f58b31b7b13b157
unknown <C:\Users\USER\AppData\Roaming\The Bat!>
2024-09-24 18:18:18 +0500

    oxirgi variant

---
+++ script.js
@@ -0,0 +1,35 @@
+var stop = true;
+const baza = [];
+
+while (stop) {
+  const text = prompt(
+    "Komanda va matn kiriting. (add, Ali) yoki (delete, Ali) yoki (update, Ali, Abbos) yoki (stop) ko'rinishida"
+  ).split(",");
+
+  switch (text[0]) {
+    case "add":
+      baza.push(text[1]);
+      console.log(text[1] + " qo'shildi");
+      break;
+    case "delete":
+      baza.splice(
+        baza.findIndex((arr) => arr[0] == text[1]),
+        1
+      );
+      console.log(text[1] + " o'chirildi");
+      break;
+    case "update":
+      let index = baza.findIndex((arr) => arr === text[1]);
+      baza.splice(index, 1, text[2]);
+      console.log(text[1] + " " + text[2] + "ga o'zgartirildi");
+      break;
+    case "stop":
+      stop = false;
+      console.log("dastur to'xtatildi");
+      break;
+    default:
+      break;
+  }
+}
+
+console.log(baza);
